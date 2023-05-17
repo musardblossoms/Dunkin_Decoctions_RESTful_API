a111f26f18867607b360b822ca5e167d5f17d87e
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2023-05-16 20:01:35 -0400

    first commit

---
+++ src/main/java/com/magic/potions/MagicPotionsApplication.java
@@ -0,0 +1,13 @@
+package com.magic.potions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicPotionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicPotionsApplication.class, args);
+	}
+
+}
