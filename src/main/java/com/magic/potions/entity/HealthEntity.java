d391b88010bb8fe61f4dfc899fd770db358cf88f
theprophet87 <64672541+theprophet87@users.noreply.github.com>
2023-06-19 15:20:48 -0400

    added default constructor for healthEntity

--- src/main/java/com/magic/potions/entity/HealthEntity.java
+++ src/main/java/com/magic/potions/entity/HealthEntity.java
@@ -2,14 +2,19 @@ package com.magic.potions.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "health_potions")
 public class HealthEntity {
+
 
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
